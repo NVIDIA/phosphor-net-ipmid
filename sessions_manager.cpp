f01b2282a87e6eb94af10bd4cb50ed7322bed2ed
Deepak Kodihalli <deepak.kodihalli.83@gmail.com>
2022-09-27 19:05:13 +0530

    Revert "Session cleaner is active until sessions close"
    This reverts commit/Change-Id 2528dfbdfdac5e0167d6529a25ee12b556577e1a.

--- sessions_manager.cpp
+++ sessions_manager.cpp
@@ -87,9 +87,6 @@ std::shared_ptr<Session>
     std::shared_ptr<Session> session = nullptr;
     SessionID bmcSessionID = 0;
     cleanStaleEntries();
-    // set up the timer for monitoring this session
-    scheduleSessionCleaner(std::chrono::microseconds(1 * 1000 * 1000));
-
     uint8_t sessionHandle = 0;
 
     auto activeSessions = sessionsMap.size() - session::maxSessionlessCount;
@@ -267,11 +264,6 @@ void Manager::cleanStaleEntries()
         }
         if (!(session->isSessionActive(activeGrace, setupGrace)))
         {
-            log<level::INFO>(
-                "Removing idle IPMI LAN session",
-                entry("SESSION_ID=%x", session->getBMCSessionID()),
-                entry("HANDLE=%x",
-                      getSessionHandle(session->getBMCSessionID())));
             sessionHandleMap[getSessionHandle(session->getBMCSessionID())] = 0;
             iter = sessionsMap.erase(iter);
         }
@@ -282,10 +274,7 @@ void Manager::cleanStaleEntries()
     }
     if (sessionsMap.size() > 1)
     {
-        constexpr int maxCleanupDelay = 1 * 1000 * 1000;
-        std::chrono::microseconds cleanupDelay(
-            std::min(setupMicros, maxCleanupDelay));
-        scheduleSessionCleaner(cleanupDelay);
+        scheduleSessionCleaner(setupGrace);
     }
 }
 
@@ -341,14 +330,6 @@ uint8_t Manager::getActiveSessionCount() const
 
 void Manager::scheduleSessionCleaner(const std::chrono::microseconds& when)
 {
-    std::chrono::duration expTime = timer.expires_from_now();
-    if (expTime > std::chrono::microseconds(0) && expTime < when)
-    {
-        // if timer has not already expired AND
-        //    requested timeout is greater than current timeout
-        // then ignore this new requested timeout
-        return;
-    }
     timer.expires_from_now(when);
     timer.async_wait([this](const boost::system::error_code& ec) {
         if (!ec)
