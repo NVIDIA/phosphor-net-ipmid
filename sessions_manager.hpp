f33ba5c1a1bacd052d4d97b017aef991326eb418
leonhuang <leonhuang@nvidia.com>
2025-04-21 19:56:34 -0700

    [GB200NVL] check incomplete session
    Whenever a session from RAKP12 times out,
    we send a PAM authentication with an empty password.
    If a response is received from the remote side and
    it reaches the RAKP34 phase, we send a
    PAM authentication with the correct password.
    This allows user lockout to work and aligns with the Redfish mechanism.
    
    Enhancement:
    1. Add redfish logging event when user lockup.
    2. Remove checking userData size.
    3. Remove unused structure.
    4. Put the ++it in the for loop
    5. Use maybe_unused on userName when PAM_AUTHENTICATE is not enabled
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5111389
    
    Signed-off-by: leonhuang@nvidia.com

--- sessions_manager.hpp
+++ sessions_manager.hpp
@@ -12,6 +12,8 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace session
 {
@@ -129,6 +131,10 @@ class Manager
      */
     void scheduleSessionCleaner(const std::chrono::microseconds& grace);
 
+    bool handleRAKP12(uint8_t userId, uint32_t sessionId);
+    bool handleRAKP34(uint8_t userId, uint32_t sessionId,
+                      [[maybe_unused]] const std::string& userName);
+
   private:
     /**
      * @brief reclaim system resources by limiting idle sessions
@@ -162,6 +168,8 @@ class Manager
     std::string chName{}; // Channel Name
     uint8_t ipmiNetworkInstance = 0;
     void setNetworkInstance(void);
+
+    std::unordered_map<uint8_t, std::vector<uint32_t>> userSessionMap;
 };
 
 } // namespace session
